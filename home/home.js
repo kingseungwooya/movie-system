c332007159b072ba7bebf6a71e870fba25af32d2
kingseungwooya <ksw_0505@nate.com>
2022-12-04 19:55:19 +0900

    feat: 로그인 후 output

--- home/home.js
+++ home/home.js
@@ -18,10 +18,38 @@ submitBt.addEventListener("click", function() {
   var inputId = document.getElementById("loginId").value;
   var inputPw = document.getElementById("loginPw").value;
   if(inputValidation(inputId, inputPw)) {
+    // 기존 노드
+    var openBtn = document.querySelector(".openBtn");
+
+    // 교체할 노드 만들기
+    var newOpenBt = document.createElement("button");
+    var textNew = document.createTextNode("로그아웃");
+    newOpenBt.setAttribute("class","openBtn");
+    newOpenBt.appendChild(textNew);
+
+    // 노드 교체
+    openBtn.replaceWith(newOpenBt);
+
+    // 버튼 앞에 아이디 추가 
+    var textNode = document.createTextNode(inputId);
+    var topBar = document.getElementById("topBar");
+
+    appendFirst(topBar, textNode);
+   
     
 
   }
 })
+
+
+function appendFirst(node, childNode) {
+
+  if (node.firstChild) {
+      node.insertBefore(childNode, node.firstChild);
+  } else {
+      node.appendChild(childNode);
+  }
+};
 
 // 회원가입 
 signInBt.addEventListener("click", function() {
