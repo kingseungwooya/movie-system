ff163c90250a64e13970d16ad6bce851c173030b
kingseungwooya <ksw_0505@nate.com>
2022-12-04 18:56:53 +0900

    feat: 회원 입력양식 검증 기능

--- home/home.js
+++ home/home.js
@@ -9,3 +9,42 @@ const close = () => {
 document.querySelector(".openBtn").addEventListener("click", open);
 document.querySelector(".closeBtn").addEventListener("click", close);
 document.querySelector(".bg").addEventListener("click", close);
+
+
+var inputId = document.getElementById("loginId");
+var inputPw = document.getElementById("loginPw");
+var submitBt = document.getElementById("submitBt"); 
+var signInBt = document.getElementById("signInBt");
+
+// 로그인
+submitBt.addEventListener("click", function() {
+  if(inputValidation(inputId, inputPw)) {
+
+  }
+})
+
+// 회원가입 
+signInBt.addEventListener("click", function() {
+  if(inputValidation(inputId, inputPw)) {
+    
+  }
+})
+
+// 검증
+function inputValidation(inputId, inputPw) {
+  var patternId = "^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$";
+  
+  var patternPw = ": ^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$";
+
+  var message = "아이디 또는 패스워드의 입력 양식을 체크해주세요";
+
+  if(!patternId.test(inputId)) {
+    alert(message)
+    return false;
+  }
+  if(!patternPw.test(inputPw)) {
+    alert(message)
+    return false;
+  }
+  return true;
+}
